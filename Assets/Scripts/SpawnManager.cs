f819cf08f834d93087298c96b4fdd56188322afa
Lâm Tiên Hưng <Hungltse170216@fpt.edu.vn>
2024-01-09 19:16:07 +0700

    7.5 Tidy up the Spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,7 +24,7 @@ public class SpawnManager : MonoBehaviour
     {
         while (true)
         {
-            Instantiate(_enemyPrefab, new Vector3(Random.Range(-8f, 8f), 7f, 0), Quaternion.identity);
+            Instantiate(_enemyPrefab, new Vector3(Random.Range(-8f, 8f), 7f, 0), Quaternion.identity, _enemyContainer.transform);
             yield return new WaitForSeconds(5f);
         }
     }
