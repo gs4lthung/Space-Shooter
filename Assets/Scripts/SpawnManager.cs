574c464b6c88c1fa4ecdca079a666d588733e5e5
Lâm Tiên Hưng <Hungltse170216@fpt.edu.vn>
2024-01-09 18:56:59 +0700

    7.4 Spawn Routine

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            Instantiate(_enemyPrefab, new Vector3(Random.Range(-8f, 8f), 7f, 0), Quaternion.identity);
+            yield return new WaitForSeconds(5f);
+        }
     }
 }
