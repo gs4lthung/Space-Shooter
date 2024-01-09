477b272f4bff5422ca91aa62ec141b27a78557a3
Lâm Tiên Hưng <Hungltse170216@fpt.edu.vn>
2024-01-09 20:29:25 +0700

    8.5 Laser 3D to 2D

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -52,7 +52,7 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
             _canFire = Time.time + _fireRate;
-            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1f, 0), Quaternion.identity);
         }
     }
     public void Damage()
